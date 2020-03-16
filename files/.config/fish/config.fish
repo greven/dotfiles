50e6cae72551e4294f80cbc9c772eec4d569bf26
Nuno Freire <greven.pt@gmail.com>
2020-03-16 18:27:38 +0000

    Add Elixir libs env

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -19,6 +19,8 @@ nvm use default --silent
 # Add .local bin to PATH
 set PATH {$HOME}/.local/bin $PATH
 
+# Elixir
+set -x ERL_LIBS /usr/lib/elixir/lib
+
 # MacOS
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
-
