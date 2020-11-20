c4baa6b73a9cf413ed5b2a821d7f46437b79da23
Nuno Freire <greven.pt@gmail.com>
2020-11-20 16:59:31 +0000

    Update fish

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,23 +1,26 @@
-. ~/.config/fish/aliases.fish
-
 # Add .local bin to PATH
 set PATH {$HOME}/.local/bin $PATH
 
 # Elixir
 set -x ERL_LIBS /usr/lib/elixir/lib
 
-# Linuxbrew
-set -gx HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
-set -gx HOMEBREW_CELLAR /home/linuxbrew/.linuxbrew/Cellar
-set -gx HOMEBREW_REPOSITORY /home/linuxbrew/.linuxbrew/Homebrew
-set -g fish_user_paths /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin $fish_user_paths
-set -q MANPATH
-or set MANPATH ''
-set -gx MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
-set -q INFOPATH
-or set INFOPATH ''
-set -gx INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
-
 # MacOS
 test -e {$HOME}/.iterm2_shell_integration.fish
 and source {$HOME}/.iterm2_shell_integration.fish
+
+## Aliases
+
+# Shell
+alias l 'ls -alhF'
+
+# Python as Python3
+alias python python3
+alias pip pip3
+
+# Scripts
+alias wth 'git-what-the-hell-just-happened.sh'
+
+# Apps
+alias g git
+alias docker-daemon 'open --background -a Docker'
+alias neo 'neofetch --block_range 0 15 --off'
